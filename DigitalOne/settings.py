2d312096d961ad363465db3e80518a9eb619d703
Mustafizur Rahman <sojibvikings@gmail.com>
2022-01-19 21:21:18 +0600

    static

--- DigitalOne/settings.py
+++ DigitalOne/settings.py
@@ -116,8 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-# Default primary key field type
+STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
