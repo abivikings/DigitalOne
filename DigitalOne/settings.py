14f4b1c265de6a6adfd85646c15933352fc6ec54
Mustafizur Rahman <sojibvikings@gmail.com>
2022-01-20 21:13:07 +0600

    assets

--- DigitalOne/settings.py
+++ DigitalOne/settings.py
@@ -115,8 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
