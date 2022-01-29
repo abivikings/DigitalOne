095f3442ee2ecfb322e32d28ac5ecce31ade48af
Mustafizur Rahman <sojibvikings@gmail.com>
2022-01-29 22:19:00 +0600

    about us

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from website.views import *
 urlpatterns = [
     path('', Home, name='home'),
+    path('about_us/', AboutUs, name='about_us'),
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     return render(request, 'website/home.html')
+
+
+def AboutUs(request):
+    return render(request, 'website/about_us.html')
