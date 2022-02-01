b6b28299a7c97f3589d66b18e07b31300805ecd7
Mustafizur Rahman <sojibvikings@gmail.com>
2022-02-01 21:16:13 +0600

    calendar event

--- DigitalOne/urls.py
+++ DigitalOne/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path, include
+from .views import DashboardView
+from django.contrib import admin
+
 urlpatterns = [
-    path('', include('website.urls'))
+    path('', include('website.urls')),
+    path("", DashboardView.as_view(), name="dashboard"),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("calender/", include("calendarapp.urls")),
 ]
